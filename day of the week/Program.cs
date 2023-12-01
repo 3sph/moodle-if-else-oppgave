41b066616b30912bc5b6f1ae0d6123afb6c44d81
3sph <141910502+3sph@users.noreply.github.com>
2023-12-01 14:35:40 +0100

    Add project files.

---
+++ day of the week/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Runtime.CompilerServices;
+using day_of_the_week;
+
+while(true)
+{
+Console.WriteLine("Lets insert a number between 1 and 7 to see what day of the week it is.");
+var input = Console.ReadLine();
+
+var days = new Days();
+
+switch (input)
+{
+    
+case "1": 
+    Console.WriteLine($"the number you have selected corresponds to {days.Monday}");
+    break;
+    case "2":
+    Console.WriteLine($"the number you have selected corresponds to {days.Tuesday}");
+    break;
+    case "3":
+    Console.WriteLine($"the number you have selected corresponds to {days.Wednesday}");
+    break;
+    case "4":
+    Console.WriteLine($"the number you have selected corresponds to {days.Thursday}");
+    break;
+    case "5":
+    Console.WriteLine($"the number you have selected corresponds to {days.Friday}");
+    break;
+    case "6":
+    Console.WriteLine($"the number you have selected corresponds to {days.Saturday}");
+    break;
+    case "7":
+    Console.WriteLine($"the number you have selected corresponds to {days.Sunday}");
+    break;
+        case "0":
+            return;
+
+default: Console.WriteLine("the number you have selected does not correspond to a day"); 
+    break;
+
+}
+}
---
+++ day of the week/days.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day_of_the_week
+{
+    internal class Days
+    {
+        public string Monday = "Monday";
+        public string Tuesday = "Tuesday";
+        public string Wednesday = "Wednesday";
+        public string Thursday = "Thursday";
+        public string Friday = "Friday";
+        public string Saturday = "Saturday";
+        public string Sunday = "Sunday";
+    }
+}
